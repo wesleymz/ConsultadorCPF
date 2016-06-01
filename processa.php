20004a26fdff15abdd1948435d5886b0de47e3ad
hidekiEduardo <eduardo.nagata@fatec.sp.gov.br>
2016-06-01 18:18:29 -0300

    Update processa.php

--- processa.php
+++ processa.php
@@ -1,12 +1,16 @@
 <?php
 
 require("Consultador.php");
-
+$html = "";
 if (isset($_POST["cnpj"]))
 {
 	$cnpj = $_POST["cnpj"];
 	$captcha = $_POST["captcha"];
 	$html = Consultador::getHtmlCNPJ($cnpj, $captcha);
+	
+	// Recebe nome se for valido, ou mensagem de erro se CNPJ for inválido ou inexistente.
+	$output = Consultador::extrairNomeEmpresarial($html);
+	//print_r($output);
 	
 	// Retorna ao formulario de consulta caso o captcha digitado for incorreto.
 	if (strpos($html, "<b>Erro na Consulta</b>"))
@@ -22,13 +26,16 @@ if (isset($_POST["cpf"]) && isset($_POST["data_nasc"]))
 	$captcha = $_POST["captcha"];
 	$html = Consultador::getHtmlCpf($cpf, $data_nasc, $captcha);
 
+	// Recebe nome se cpf for válido, ou mensagem de erro se for inválido ou inexistente.
+	$output = Consultador::extrairNomePessoaFisica($html);
+	//print_r($output);
+	
 	// Retorna ao formulario de consulta caso o captcha esteja incorreto.
-	if (strpos($html, "Os caracteres da imagem não foram preenchidos corretamente."))
+	if (strpos($html, "Por favor, envie novamente.") || strpos($html, "Por favor, preencha os dados novamente."))
 	{
 		header("Location: consulta.php?numero=" . $_POST["cpf"] . "&data_nasc=" . $_POST["data_nasc"]);
 	}
 }
-
 
 print $html;
 
